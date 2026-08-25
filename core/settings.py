7fd17993fa9da03c17029563000b1b794b412889
snowmanly101 <dbtttttttfoot@gmail.com>
2026-08-25 15:30:28 -0700

    Final fix for email environment variables

--- core/settings.py
+++ core/settings.py
@@ -121,8 +121,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # --- EMAIL CONFIGURATION FOR VERIFICATION CODES ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'auramatchdate@gmail.com'
-EMAIL_HOST_PASSWORD = 'pnlc gpie vcvd bzjq'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'mail.privateemail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 465))
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'True') == 'True'
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'False') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'service@auramatch.forum')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
