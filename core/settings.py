3ffceeec61a7fa4b53a9e2c4e3fddde03599b62b
snowmanly101 <dbtttttttfoot@gmail.com>
2026-08-25 17:35:58 -0700

    Fix settings file with clean complete script

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Recommended for Render static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
