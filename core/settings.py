f74078bd8b670995267e39a569f27e73e7508725
snowmanly101 <dbtttttttfoot@gmail.com>
2026-09-02 09:18:15 -0700

    Fix file path double underscore typo

--- core/settings.py
+++ core/settings.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(_file_).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-replace-me-with-a-secure-key')
